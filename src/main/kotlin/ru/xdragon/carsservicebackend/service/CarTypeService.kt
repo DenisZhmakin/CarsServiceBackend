bb414f8fb7cb35a5d852a59cc233ed331d2b2812
DenisZhmakin <den.zhmakin.00@gmail.com>
2024-11-28 13:23:35 +0300

    feat: Доделал формирование списка типов машин.

--- src/main/kotlin/ru/xdragon/carsservicebackend/service/CarTypeService.kt
+++ src/main/kotlin/ru/xdragon/carsservicebackend/service/CarTypeService.kt
@@ -1,13 +1,18 @@
 package ru.xdragon.carsservicebackend.service
 
-import CarTypeServiceOuterClass.CarTypeRequest
-import CarTypeServiceOuterClass.CarTypeResponse
 import io.grpc.stub.StreamObserver
 import net.devh.boot.grpc.server.service.GrpcService
 import org.springframework.beans.factory.annotation.Autowired
+
 import ru.xdragon.carsservicebackend.entity.CarTypeEntity
 import ru.xdragon.carsservicebackend.repository.CarTypeRepository
+
+import CarTypeServiceOuterClass.CarTypeRequest
+import CarTypeServiceOuterClass.CarTypeResponse
+import CarTypeServiceOuterClass.CarTypeEmptyRequest
+import CarTypeServiceOuterClass.ListCarTypeResponse
 import CarTypeServiceGrpc.CarTypeServiceImplBase as CarTypeServiceBase
+
 
 @GrpcService
 class CarTypeService : CarTypeServiceBase() {
@@ -25,6 +30,28 @@ class CarTypeService : CarTypeServiceBase() {
             .newBuilder()
             .setId(carTypeEntity.id!!)
             .setValue(carTypeEntity.value)
+            .build()
+
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+
+    override fun getAllCarTypes(request: CarTypeEmptyRequest, responseObserver: StreamObserver<ListCarTypeResponse>) {
+        val result: MutableList<CarTypeResponse> = mutableListOf()
+
+        carTypeRepository.findAll().forEach { carTypeEntity ->
+            result.add(
+                CarTypeResponse
+                    .newBuilder()
+                    .setId(carTypeEntity.id!!)
+                    .setValue(carTypeEntity.value)
+                    .build()
+            )
+        }
+
+        val response = ListCarTypeResponse
+            .newBuilder()
+            .addAllCarTypes(result)
             .build()
 
         responseObserver.onNext(response)
